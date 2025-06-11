98f859a27da409c84d0e728795137e3dd9f0ad84
truman <trumanjimothy@gmail.com>
2025-06-11 16:35:25 +0900

    Added and linked settings window

--- Godot Project/Scripts/SplashScreen/SplashScreen.cs
+++ Godot Project/Scripts/SplashScreen/SplashScreen.cs
@@ -6,6 +6,6 @@ public partial class SplashScreen : TextureRect
 	public override void _Ready()
 	{
 		GetNode<Timer>("Timer").Timeout += ()
-		=> GetNode<SceneLoader>("res://Scenes/SceneLoader/scene_loader.tscn/SceneLoader").ChangeToScene("title_screen.tscn");
+		=> GetNode<SceneLoader>("/root/SceneLoader").ChangeToScene("title_screen.tscn");
 	}
 }
---
+++ Godot Project/Scripts/TitleScreen/TitleScreen.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public partial class TitleScreen : TextureRect
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		GetNode<Button>("ButtonContainer/ButtonBox/NewGame").Pressed +=
+			() => GetNode<SceneLoader>("/root/SceneLoader").ChangeToScene("settings_window.tscn");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
