f853ed587634593b9cf063d17589492ab1d2dc94
truman <trumanjimothy@gmail.com>
2025-06-19 20:51:02 +0900

    added transition for getting to dialogue scenes

--- Godot Project/Scripts/Safehouse/Safehouse.cs
+++ Godot Project/Scripts/Safehouse/Safehouse.cs
@@ -9,16 +9,22 @@ public partial class Safehouse : StaticBody2D
 
 	//RayCast2D _ray; - May come back to this, for now ignore all the ray stuff
 
-	float _day_num; // Keep track of day, mornigns will be whole numbers, nights will be X.5
+	float _day_num; // Keep track of day, mornings will be whole numbers, nights will be X.5
 
 	// Flags to see if the player is in an interactable area
 	bool _in_bed = false;
 	bool _at_door = false;
 	bool _at_table = false;
 
+	// Flags for enviroment
+	bool _npc_waiting = false;
+	String[] _character_order = ["old_man_tutorial_dialogue", "mom_dialogue", "Kaishain", "Kid", "OldManEnd"];
+
+	bool _player_has_cards = false;
+
 	// Will be prompt nodes
 	Control _end_day_prompt;
-	Control _go_outside_prompt;
+	Control _open_door_prompt;
 	Control _start_game_prompt;
 
 	// Called when the node enters the scene tree for the first time.
@@ -26,7 +32,7 @@ public partial class Safehouse : StaticBody2D
 	{
 		// Get all the prompt nodes
 		_end_day_prompt = GetNode<Control>("EndDayPrompt");
-		_go_outside_prompt = GetNode<Control>("GoOutsidePrompt");
+		_open_door_prompt = GetNode<Control>("OpenDoorPrompt");
 		_start_game_prompt = GetNode<Control>("StartGamePrompt");
 
 		//_ray = GetNode<RayCast2D>("PlayerCharacter/RayCast2D");
@@ -42,7 +48,17 @@ public partial class Safehouse : StaticBody2D
 			// If the player is in an interactable area, then show the prompt for that object
 			if (_at_table)
 			{
-				_start_game_prompt.Visible = true;
+				if (_player_has_cards)
+				{
+					_start_game_prompt.Visible = true;
+				}
+
+				else
+				{
+					GetNode<TextureRect>("MenkoCards").Visible = false;
+					_player_has_cards = true;
+				}
+
 			}
 
 			else if (_in_bed)
@@ -52,7 +68,10 @@ public partial class Safehouse : StaticBody2D
 
 			else if (_at_door)
 			{
-				_go_outside_prompt.Visible = true;
+				if (!_npc_waiting)
+				{
+					_open_door_prompt.Visible = true;
+				}
 			}
 		}
 	}
@@ -92,18 +111,36 @@ public partial class Safehouse : StaticBody2D
 	void _on_cancel_pressed()
 	{
 		_end_day_prompt.Visible = false;
-		_go_outside_prompt.Visible = false;
+		_open_door_prompt.Visible = false;
 		_start_game_prompt.Visible = false;
+	}
+
+	// When the player opens the door for the NPC
+	void _on_open_door_pressed()
+	{
+		GetNode<AnimationPlayer>("FadeToBlack/AnimationPlayer").Play("fade_to_dialogue");
 	}
 
 	// Start Menko Game
 	void _on_start_game_pressed()
 	{
-		GetNode<AnimationPlayer>("FadeToBlack/AnimationPlayer").Play("fade_to_black");
+		GetNode<AnimationPlayer>("FadeToGame/AnimationPlayer").Play("fade_to_game");
 	}
 
 	void _on_animation_player_animation_finished(StringName anim_name)
 	{
-		GetNode<SceneLoader>("/root/SceneLoader").ChangeToScene("menko_game.tscn");
+		if (anim_name == "fade_to_game")
+		{
+			GetNode<SceneLoader>("/root/SceneLoader").ChangeToScene("menko_game.tscn");
+		}
+		else if (anim_name == "fade_to_dialogue")
+		{
+			GetNode<SceneLoader>("/root/SceneLoader").ChangeToScene($"{_character_order[(int)_day_num]}.tscn");
+		}
+	}
+
+	void _knock_at_door()
+	{
+		_npc_waiting = true;
 	}
 }
